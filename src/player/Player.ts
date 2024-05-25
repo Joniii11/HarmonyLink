8f4220a582c719e1af59f38d0ffa5cc89ecc7f9e
Jonas <jonasdimpfl@gmail.com>
2024-05-25 13:58:55 +0200

    Create Player class

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -1,1 +1,10 @@
+import { EventEmitter } from "events"
+
 import type { HarmonyLink } from "@/HarmonyLink"
+
+export class Player extends EventEmitter {
+    public node: Node;
+    public manager: HarmonyLink;
+
+    
+};
